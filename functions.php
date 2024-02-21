4e3f8a5e721159c3dd8051867b5c834d9fa8d51c
emilianogastaldo <emiliano0303@gmail.com>
2024-02-21 16:34:40 +0100

    now the password don't have same letter

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 function random_char($length){
     $rand_word='';
     while(strlen($rand_word) < $length){
-        $rand_word .= chr(rand(33,126));
+        $rand_letter = chr(rand(33,126));
+        if(!str_contains($rand_word, $rand_letter)) $rand_word .= $rand_letter;
     }
     return $rand_word;
 }
